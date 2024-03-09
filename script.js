fedb6647acc3fe99b0c71958467f75ccd689d7b9
Luke Wheldale <lukewheldale@gmail.com>
2024-03-09 17:04:53 +1030

    game function is now IIFE

--- script.js
+++ script.js
@@ -42,44 +42,32 @@ function determineWinner() {
 }
 determineWinner();
 
-// Checking if board has 'x' or 'o' in the winning conditions subarrays
-
-// function placeToken(token) {
-// 	// if (
-// 	// 	Gameboard.board[token] == Gameboard.board["x"] ||
-// 	// 	Gameboard.board[token] == Gameboard.board["o"]
-// 	// ) {
-// 	// 	console.log("that space is taken");
-// 	// }
-// 	return {
-
-//     }
-// }
-
-const game = {
-	// Render the gameboard to DOM
-	renderBoard: () => {
-		for (const key in cells) {
-			cells[key].textContent = Gameboard.board[key];
-		}
-	},
-	// Determine play turn, take turn by placing marker if cell is empty then switch players turn
-	handlePlayerTurn: function (position) {
-		if (Gameboard.board[position] == null) {
-			if (playerOne.isTurn == true) {
-				Gameboard.board[position] = playerOne.marker;
-				playerOne.isTurn = false;
-				playerTwo.isTurn = true;
-			} else if (playerTwo.isTurn == true) {
-				Gameboard.board[position] = playerTwo.marker;
-				playerTwo.isTurn = false;
-				playerOne.isTurn = true;
+const Game = (function () {
+	return {
+		// Render the gameboard to DOM
+		renderBoard: () => {
+			for (const key in cells) {
+				cells[key].textContent = Gameboard.board[key];
 			}
-		}
-		renderBoard();
-	},
-};
+		},
+		// Determine play turn, take turn by placing marker if cell is empty then switch players turn
+		handlePlayerTurn: function (position) {
+			if (Gameboard.board[position] == null) {
+				if (playerOne.isTurn == true) {
+					Gameboard.board[position] = playerOne.marker;
+					playerOne.isTurn = false;
+					playerTwo.isTurn = true;
+				} else if (playerTwo.isTurn == true) {
+					Gameboard.board[position] = playerTwo.marker;
+					playerTwo.isTurn = false;
+					playerOne.isTurn = true;
+				}
+			}
+			renderBoard();
+		},
+	};
+})();
 
-game.handlePlayerTurn(6);
-game.handlePlayerTurn(0);
-game.handlePlayerTurn(5);
+Game.handlePlayerTurn(6);
+Game.handlePlayerTurn(0);
+Game.handlePlayerTurn(5);
